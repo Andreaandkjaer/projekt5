ba4ccbd306f9473d0eb6f124110b5f8caa31cef0
JulietDane <julieringivehansen@outlook.com>
2023-05-28 14:49:26 +0200

    5

--- pentia.js
+++ pentia.js
@@ -15,6 +15,25 @@ for (i = 0; i < button.length; i++) {
 }
 /*Andrea JavaScript slut*/
 /*Julie JavaScript start*/
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // Skifter billeder hver 3 sek.
+}
 /*Julie JavaScript slut*/
 /*Michelle JavaScript start*/
 /*Michelle JavaScript slut*/
